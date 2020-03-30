ce357760166af79b920ad4d732af946dfd6518a5
mohan145 <mohan145krsihna@gmail.com>
2020-03-31 02:22:54 +0530

    deleted android app, initialzed node project with package.json

--- app/src/main/java/com/sarath/dev/covid/controllers/network/Endpoints.kt
+++
@@ -1,7 +0,0 @@
-package com.sarath.dev.covid.controllers.network
-
-class Endpoints {
-    companion object {
-        const val SUMMARY = "summary"
-    }
-}
--- app/src/main/java/com/sarath/dev/covid/controllers/utils/Info.kt
+++
@@ -1,7 +0,0 @@
-package com.sarath.dev.covid.controllers.utils
-
-class Info(
-    var totalCases: Int?,
-    var newCases: Int?,
-    var type: String?
-)
